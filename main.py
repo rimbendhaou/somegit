adab385567b9af14a604be4c3e12c75bb10f656d
rim <bendhaou.rim@gmail.com>
2021-02-23 19:53:08 -0500

    my first feature

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def print_hi(name):
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+# this is for commit number one
 if __name__ == '__main__':
     print_hi('PyCharm')
